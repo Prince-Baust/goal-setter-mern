1f2fbc09537bd3c928ba11a88a984e850ebb5a04
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-09 23:40:17 +0600

    error handling. error overwriting left for later debugging

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -11,9 +11,8 @@ const getGoals = (req, res) => {
 const setGoals = (req, res) => {
   if(!req.body.text) {
     res.status(400)
-    throw new Error('Please add a text field')
+    throw new Error('Why not add a text field')
   }
-  console.log(req.body)
   res.status(200).json({message: 'Set Goals'})
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const {errorHandler} = require("./middleware/errorMiddleware");
 const dotenv = require('dotenv').config();
-const {errorHandler} = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -8,6 +8,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+
+// TODO: debug error overwriting issue
 app.use(errorHandler);
 
 app.listen(port, ()=> console.log(`Server started at ${port}`));
