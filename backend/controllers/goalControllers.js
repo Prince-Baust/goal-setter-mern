a3a749f8c3042708b975f59b42930fe873774db1
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-10 00:49:23 +0600

    delete goal implemented in db

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -48,7 +48,16 @@ const updateGoal = asyncHandler(async (req, res) => {
 // @route     DELETE /api/goals/:id
 // @access    Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({message: `Delete goal ${req.params.id}`})
+  const goal = await Goal.findById(req.params.id);
+
+  if(!goal) {
+    res.status(400);
+    throw new Error('Goal Not Found');
+  }
+
+  await goal.remove();
+
+  res.status(200).json({id: req.params.id});
 })
 
 module.exports = {
