d1ef199d72de4cbfd584f1526ed424c64341c115
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-10 00:42:20 +0600

    update goal implemented in db

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -25,12 +25,24 @@ const setGoals = asyncHandler(async (req, res) => {
   res.status(200).json(goal);
 })
 
+
 // @desc      Update goals
 // @route     PUT /api/goals/:id
 // @access    Private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({message: `Update goal ${req.params.id}`})
+  const goal = await Goal.findById(req.params.id);
+
+  if(!goal) {
+    res.status(400);
+    throw new Error('Goal Not Found!');
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+
+  res.status(200).json(updatedGoal);
 })
+
 
 // @desc      Delete goals
 // @route     DELETE /api/goals/:id
