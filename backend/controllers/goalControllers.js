5405cf5c385afe0d7d999525e43df93c69bf72da
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-09 19:56:26 +0600

    reading body data

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -9,6 +9,11 @@ const getGoals = (req, res) => {
 // @route     POST /api/goals
 // @access    Private
 const setGoals = (req, res) => {
+  if(!req.body.text) {
+    res.status(400)
+    throw new Error('Please add a text field')
+  }
+  console.log(req.body)
   res.status(200).json({message: 'Set Goals'})
 }
 
