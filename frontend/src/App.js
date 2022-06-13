a015fd2a17753ca9fe986c0a1af98cbdaba4e873
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-13 18:03:30 +0600

    react router installed and pages setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
