815e2b22f8f71a162101f43101b66868e8a2f87a
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-13 20:18:33 +0600

    register page complete

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,77 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {FaUser} from "react-icons/fa";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const {name, email, password, password2} = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>
-      Register
-    </div>
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser/> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input type="text" className="form-control"
+                   id='name'
+                   name='name'
+                   value={name}
+                   placeholder="Enter Your Name"
+                   onChange={onChange}/>
+          </div>
+          <div className="form-group">
+            <input type="email" className="form-control"
+                   id='email'
+                   name='email'
+                   value={email}
+                   placeholder="Enter Your email"
+                   onChange={onChange}/>
+          </div>
+          <div className="form-group">
+            <input type="password" className="form-control"
+                   id='password'
+                   name='password'
+                   value={password}
+                   placeholder="Enter Your password"
+                   onChange={onChange}/>
+          </div>
+          <div className="form-group">
+            <input type="password" className="form-control"
+                   id='password2'
+                   name='password2'
+                   value={password2}
+                   placeholder="Enter Your password Again"
+                   onChange={onChange}/>
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
   );
 };
 
