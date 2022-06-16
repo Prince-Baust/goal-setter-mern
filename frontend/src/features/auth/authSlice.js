76286297c6e726e193fe3471889aeefcf27e348e
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-16 20:47:17 +0600

    registration complete

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,10 +1,11 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 import authService from "./authService";
+
 // Get user from localStorage
 const user = JSON.parse(localStorage.getItem('user'))
 
 const initialState = {
-  user: user ? user: null,
+  user: user ? user : null,
   isError: false,
   isSuccess: false,
   isLoading: false,
@@ -16,13 +17,13 @@ export const register = createAsyncThunk('auth/register', async (user, thunkAPI)
   try {
     return await authService.register(user)
   } catch (error) {
-    const message = (error.response && error.response.data && error.response.data.message || error.message || error.toString())
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
 
     return thunkAPI.rejectWithValue(message)
   }
 })
 
-export const authSlice = createSlice( {
+export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
@@ -39,12 +40,12 @@ export const authSlice = createSlice( {
       .addCase(register.pending, (state) => {
         state.isLoading = true
       })
-      .addCase(register.fulfilled, (state,action) => {
+      .addCase(register.fulfilled, (state, action) => {
         this.state.isLoading = false
         this.state.isSuccess = true
         this.state.user = action.payload
       })
-      .addCase(register.rejected, (state,action) => {
+      .addCase(register.rejected, (state, action) => {
         this.state.isLoading = false
         this.state.isError = true
         this.state.message = action.payload
