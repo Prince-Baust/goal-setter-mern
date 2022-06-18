dca56e47dd1211ed777dc8fa1c3ffdfa1aaa52e1
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-18 21:26:26 +0600

    commit for production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const connectDB = require('./config/db');
@@ -13,6 +14,15 @@ app.use(express.urlencoded({extended: false}));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+//Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 // TODO: debug error overwriting issue
 app.use(errorHandler);
